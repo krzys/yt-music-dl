9927b70a56dbd100c558cd90ce48de0761900c10
Krzysztof Sternik <sternik15@gmail.com>
2020-01-03 20:09:12 +0100

    Add test to Request resulting in null

--- src/test/java/com/github/krzsernik/ytmusicdl/RequestTest.java
+++ src/test/java/com/github/krzsernik/ytmusicdl/RequestTest.java
@@ -1,26 +1,26 @@
 package com.github.krzsernik.ytmusicdl;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class RequestTest {
     public Request requestGet;
     public Request requestPost;
 
-    @Before
-    public void setup() {
-        requestGet = new Request("http://httpbin.org/get", Request.Method.GET);
-        requestPost = new Request("http://httpbin.org/post", Request.Method.POST);
-    }
-
     @Test
     public void testBasicRequests() {
+        requestGet = new Request("http://httpbin.org/get", Request.Method.GET);
         String result = requestGet.send();
         assertNotNull("Basic GET Request", result);
 
+        requestPost = new Request("http://httpbin.org/post", Request.Method.POST);
         result = requestPost.send();
         assertNotNull("Basic POST Request", result);
+
+        requestGet = new Request("http://nonexisting.domain", Request.Method.GET);
+        result = requestGet.send();
+        assertNull("Basic GET Request", result);
     }
 }
